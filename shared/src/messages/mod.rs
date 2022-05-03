a09fdcb47ff8ff5a4ef83de28b5e69536dcc0569
ElementalAlchemist <ElementAlchemist7@gmail.com>
2022-05-02 21:11:49 -0500

    Add Display for DataError so that registration errors can fully display

--- shared/src/messages/mod.rs
+++ shared/src/messages/mod.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 pub mod events;
 pub mod initial;
@@ -9,6 +10,15 @@ pub mod user_register;
 pub enum DataError {
 	DatabaseError,
 	ServerError,
+}
+
+impl fmt::Display for DataError {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Self::DatabaseError => write!(f, "A database interaction failed"),
+			Self::ServerError => write!(f, "The server failed to process")
+		}
+	}
 }
 
 pub type DataMessage<T> = Result<T, DataError>;
