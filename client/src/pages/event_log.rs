c5dec5a9d120eba2beffcaa3741028fad12d7abf
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-03-17 20:57:50 -0500

    Ignore the first run of those effects so that they do not cause everything always to count as modified
    This would defeat the purpose of separating the parts of the log entry in the first place.

--- client/src/pages/event_log.rs
+++ client/src/pages/event_log.rs
@@ -169,49 +169,94 @@ async fn EventLogLoadedView<G: Html>(ctx: Scope<'_>, props: EventLogProps) -> Vi
 					let edit_highlighted = create_signal(ctx, entry.highlighted);
 
 					let modified_data: &Signal<HashSet<ModifiedEventLogEntryParts>> = create_signal(ctx, HashSet::new());
+					let ran_once: &Signal<HashSet<ModifiedEventLogEntryParts>> = create_signal(ctx, HashSet::new());
 
 					create_effect(ctx, || {
-						let _ = edit_start_time.get();
+						edit_start_time.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::StartTime) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::StartTime);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::StartTime);
 					});
 					create_effect(ctx, || {
-						let _ = edit_end_time.get();
+						edit_end_time.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::EndTime) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::EndTime);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::EndTime);
 					});
 					create_effect(ctx, || {
-						let _ = edit_entry_type.get();
+						edit_entry_type.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::EntryType) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::EntryType);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::EntryType);
 					});
 					create_effect(ctx, || {
-						let _ = edit_description.get();
+						edit_description.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::Description) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::Description);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::Description);
 					});
 					create_effect(ctx, || {
-						let _ = edit_media_link.get();
+						edit_media_link.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::MediaLink) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::MediaLink);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::MediaLink);
 					});
 					create_effect(ctx, || {
-						let _ = edit_submitter_or_winner.get();
+						edit_submitter_or_winner.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::SubmitterOrWinner) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::SubmitterOrWinner);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::SubmitterOrWinner);
 					});
 					create_effect(ctx, || {
-						let _ = edit_tags.get();
+						edit_tags.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::Tags) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::Tags);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::Tags);
 					});
 					create_effect(ctx, || {
-						let _ = edit_make_video.get();
+						edit_make_video.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::MakeVideo) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::MakeVideo);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::MakeVideo);
 					});
 					create_effect(ctx, || {
-						let _ = edit_notes_to_editor.get();
+						edit_notes_to_editor.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::NotesToEditor) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::NotesToEditor);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::NotesToEditor);
 					});
 					create_effect(ctx, || {
-						let _ = edit_editor.get();
+						edit_editor.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::Editor) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::Editor);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::Editor);
 					});
 					create_effect(ctx, || {
-						let _ = edit_highlighted.get();
+						edit_highlighted.track();
+						if !ran_once.get_untracked().contains(&ModifiedEventLogEntryParts::Highlighted) {
+							ran_once.modify().insert(ModifiedEventLogEntryParts::Highlighted);
+							return;
+						}
 						modified_data.modify().insert(ModifiedEventLogEntryParts::Highlighted);
 					});
 
