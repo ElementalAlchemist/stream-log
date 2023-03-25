62a5cc5aa77a7c8f7749854555e1cae311d23674
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-03-25 15:02:34 -0500

    Handle updating log entry types

--- server/src/data_sync/subscriptions.rs
+++ server/src/data_sync/subscriptions.rs
@@ -470,8 +470,25 @@ pub async fn handle_event_update(
 			log_entry.end_time = new_end_time;
 			EventSubscriptionData::UpdateLogEntry(log_entry)
 		}
-		EventSubscriptionUpdate::ChangeEntryType(log_entry, new_entry_type) => {
-			todo!()
+		EventSubscriptionUpdate::ChangeEntryType(mut log_entry, new_entry_type) => {
+			{
+				let mut db_connection = db_connection.lock().await;
+				let update_result = diesel::update(event_log::table)
+					.filter(event_log::id.eq(&log_entry.id))
+					.set((
+						event_log::entry_type.eq(&new_entry_type),
+						event_log::last_update_user.eq(&user.id),
+						event_log::last_updated.eq(Utc::now()),
+					))
+					.execute(&mut *db_connection);
+				if let Err(error) = update_result {
+					tide::log::error!("Database error updating log entry type: {}", error);
+					return Ok(());
+				}
+			}
+
+			log_entry.entry_type = new_entry_type;
+			EventSubscriptionData::UpdateLogEntry(log_entry)
 		}
 		EventSubscriptionUpdate::ChangeDescription(log_entry, new_description) => {
 			todo!()
