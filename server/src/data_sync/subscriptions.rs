0a7e3c12ecb069bbc11163f76f72176ca687e653
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-03-25 19:51:25 -0500

    Handle updates to log entry make video flag

--- server/src/data_sync/subscriptions.rs
+++ server/src/data_sync/subscriptions.rs
@@ -592,8 +592,25 @@ pub async fn handle_event_update(
 			log_entry.tags = new_tags;
 			EventSubscriptionData::UpdateLogEntry(log_entry)
 		}
-		EventSubscriptionUpdate::ChangeMakeVideo(log_entry, new_make_video_value) => {
-			todo!()
+		EventSubscriptionUpdate::ChangeMakeVideo(mut log_entry, new_make_video_value) => {
+			{
+				let mut db_connection = db_connection.lock().await;
+				let update_result = diesel::update(event_log::table)
+					.filter(event_log::id.eq(&log_entry.id))
+					.set((
+						event_log::make_video.eq(new_make_video_value),
+						event_log::last_update_user.eq(&user.id),
+						event_log::last_updated.eq(Utc::now()),
+					))
+					.execute(&mut *db_connection);
+				if let Err(error) = update_result {
+					tide::log::error!("Database error updating log entry make video flag: {}", error);
+					return Ok(());
+				}
+			}
+
+			log_entry.make_video = new_make_video_value;
+			EventSubscriptionData::UpdateLogEntry(log_entry)
 		}
 		EventSubscriptionUpdate::ChangeNotesToEditor(log_entry, new_notes_to_editor) => {
 			todo!()
