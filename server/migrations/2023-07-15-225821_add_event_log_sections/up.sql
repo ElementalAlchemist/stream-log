106f6d5da501996f5721041c5448429c5c99db0d
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-07-16 10:59:25 -0500

    Add unique constraint for sections data

--- server/migrations/2023-07-15-225821_add_event_log_sections/up.sql
+++ server/migrations/2023-07-15-225821_add_event_log_sections/up.sql
@@ -2,5 +2,6 @@ CREATE TABLE event_log_sections (
 	id TEXT PRIMARY KEY,
 	event TEXT REFERENCES events NOT NULL,
 	name TEXT NOT NULL,
-	start_time TIMESTAMP WITH TIME ZONE NOT NULL
+	start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+	UNIQUE (event, name)
 );
