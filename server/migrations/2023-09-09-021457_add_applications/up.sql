82ca22235a33ca3f021fd90d27e5dde26f0d5cf6
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-09-09 12:00:51 -0500

    Add unique constraint to auth_key

--- server/migrations/2023-09-09-021457_add_applications/up.sql
+++ server/migrations/2023-09-09-021457_add_applications/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE applications (
 	id TEXT PRIMARY KEY,
 	name TEXT NOT NULL,
-	auth_key TEXT, -- A null key is a revoked application.
+	auth_key TEXT UNIQUE, -- A null key is a revoked application.
 	read_log BOOLEAN NOT NULL,
 	write_links BOOLEAN NOT NULL,
 	creation_user TEXT NOT NULL REFERENCES users
