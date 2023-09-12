7974db89c1ad9794b31bbbf3ab0ccae36645f4cf
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-09-11 23:18:48 -0500

    Document the tag API type

--- server/src/api/v1/structures/tag.rs
+++ server/src/api/v1/structures/tag.rs
@@ -1,9 +1,14 @@
 use serde::Serialize;
 
+/// Tag object representing the tag
 #[derive(Clone, Serialize)]
 pub struct Tag {
+	/// The tag's ID
 	pub id: String,
+	/// The name of the tag shown to users and used with other services
 	pub tag: String,
+	/// A description of what the tag is and how it's meant to be used
 	pub description: String,
+	/// Playlist ID, if the tag is for a playlist
 	pub playlist: Option<String>,
 }
