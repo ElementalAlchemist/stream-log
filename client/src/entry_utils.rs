18b759c54bc028bbdcd20aa1e50717b03c7e2143
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-09-24 16:26:20 -0500

    Update dependencies
    This includes one update to calls to a newly deprecated chrono function.

--- client/src/entry_utils.rs
+++ client/src/entry_utils.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
 
 pub const ISO_DATETIME_FORMAT_STRING: &str = "%Y-%m-%dT%H:%M:%S";
 
@@ -6,10 +6,10 @@ pub fn parse_time_field_value(value: &str) -> chrono::format::ParseResult<DateTi
 	// Inexplicably, browsers will just omit the seconds part even if seconds can be entered.
 	// As such, we need to handle both formats here.
 	match NaiveDateTime::parse_from_str(value, "%Y-%m-%dT%H:%M:%S") {
-		Ok(dt) => Ok(DateTime::from_utc(dt, Utc)),
+		Ok(dt) => Ok(Utc.from_utc_datetime(&dt)),
 		Err(error) => {
 			if error.kind() == chrono::format::ParseErrorKind::TooShort {
-				NaiveDateTime::parse_from_str(value, "%Y-%m-%dT%H:%M").map(|dt| DateTime::from_utc(dt, Utc))
+				NaiveDateTime::parse_from_str(value, "%Y-%m-%dT%H:%M").map(|dt| Utc.from_utc_datetime(&dt))
 			} else {
 				Err(error)
 			}
