0e10f6f0b4cbe47cf1e5ccb56c256055e0ac21c1
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-09-30 23:44:59 -0500

    Fix formatting and parsing of durations at hour negative zero

--- client/src/components/event_log_entry/utils.rs
+++ client/src/components/event_log_entry/utils.rs
@@ -2,9 +2,16 @@ use chrono::Duration;
 
 /// Formats a [`Duration`] object as hours:minutes
 pub fn format_duration(duration: &Duration) -> String {
-	let hours = duration.num_hours();
-	let minutes = (duration.num_minutes() % 60).abs();
-	format!("{}:{:02}", hours, minutes)
+	let mut hours = duration.num_hours();
+	let mut minutes = duration.num_minutes() % 60;
+	let sign = if hours < 0 || minutes < 0 {
+		hours = hours.abs();
+		minutes = minutes.abs();
+		"-"
+	} else {
+		""
+	};
+	format!("{}{}:{:02}", sign, hours, minutes)
 }
 
 /// Parses a string formatted as hhh:mm into a [`Duration`] object. If parsing fails,
@@ -14,15 +21,32 @@ pub fn get_duration_from_formatted(formatted_duration: &str) -> Result<Duration,
 		return Err(String::from("Invalid format"));
 	};
 
+	let is_negative = match hours.chars().next() {
+		Some(c) => c == '-',
+		None => false,
+	};
 	let hours: i64 = match hours.parse() {
 		Ok(hours) => hours,
 		Err(error) => return Err(format!("Couldn't parse hours: {}", error)),
 	};
 
-	let minutes: i64 = match minutes.parse() {
+	let mut minutes: i64 = match minutes.parse() {
 		Ok(mins) => mins,
 		Err(error) => return Err(format!("Couldn't parse minutes: {}", error)),
 	};
+
+	if is_negative {
+		if hours > 0 {
+			return Err(format!(
+				"Hour parsing went wrong: detected negative duration but parsed hours as {}",
+				hours
+			));
+		}
+
+		if hours == 0 {
+			minutes = -minutes;
+		}
+	}
 
 	let duration_minutes = hours * 60 + minutes;
 	Ok(Duration::minutes(duration_minutes))
