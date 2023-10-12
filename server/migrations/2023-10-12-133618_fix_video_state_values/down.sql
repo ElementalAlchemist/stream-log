c20d50c12b2aeb51cd58c9e1b88c402d440d330d
ElementalAlchemist <ElementAlchemist7@gmail.com>
2023-10-12 08:45:33 -0500

    Fix enum values for video state so that diesel actually sets to them

---
+++ server/migrations/2023-10-12-133618_fix_video_state_values/down.sql
@@ -0,0 +1,8 @@
+ALTER TYPE video_state RENAME VALUE 'unedited' TO 'UNEDITED';
+ALTER TYPE video_state RENAME VALUE 'edited' TO 'EDITED';
+ALTER TYPE video_state RENAME VALUE 'claimed' TO 'CLAIMED';
+ALTER TYPE video_state RENAME VALUE 'finalizing' TO 'FINALIZING';
+ALTER TYPE video_state RENAME VALUE 'transcoding' TO 'TRANSCODING';
+ALTER TYPE video_state RENAME VALUE 'done' TO 'DONE';
+ALTER TYPE video_state RENAME VALUE 'modified' TO 'MODIFIED';
+ALTER TYPE video_state RENAME VALUE 'unlisted' TO 'UNLISTED';
---
+++ server/migrations/2023-10-12-133618_fix_video_state_values/up.sql
@@ -0,0 +1,8 @@
+ALTER TYPE video_state RENAME VALUE 'UNEDITED' TO 'unedited';
+ALTER TYPE video_state RENAME VALUE 'EDITED' TO 'edited';
+ALTER TYPE video_state RENAME VALUE 'CLAIMED' TO 'claimed';
+ALTER TYPE video_state RENAME VALUE 'FINALIZING' TO 'finalizing';
+ALTER TYPE video_state RENAME VALUE 'TRANSCODING' TO 'transcoding';
+ALTER TYPE video_state RENAME VALUE 'DONE' TO 'done';
+ALTER TYPE video_state RENAME VALUE 'MODIFIED' TO 'modified';
+ALTER TYPE video_state RENAME VALUE 'UNLISTED' TO 'unlisted';
